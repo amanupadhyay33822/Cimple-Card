c9718591caef695d214cfe89e5be390b4f28cf56
Aman Upadhyay <amanupadhyay33822@gmail.com>
2024-12-03 23:16:27 +0530

    add rate limit

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { login, logout, sendOTP, verifyOTP } from '../controllers/auth.js';
 import { resetPassword, resetPasswordToken } from '../controllers/resetPassword.js';
+import limiter from '../middleware/ratelimiting.js';
 const router = express.Router();
 // router.post("/signup", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/send-otp", sendOTP);
+router.post("/send-otp", limiter, sendOTP);
 router.post('/verify-otp', verifyOTP);
-router.post("/send-token", resetPasswordToken);
+router.post("/send-token", limiter, resetPasswordToken);
 router.post("/updatePassword", resetPassword);
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { login, logout,sendOTP, verifyOTP } from '../controllers/auth.js';
 import { resetPassword, resetPasswordToken } from '../controllers/resetPassword.js';
+import limiter from '../middleware/ratelimiting.js';
 
 const router = express.Router();
 
@@ -9,10 +10,10 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.post("/send-otp",sendOTP)
+router.post("/send-otp",limiter,sendOTP)
 router.post('/verify-otp', verifyOTP);
 
-router.post("/send-token",resetPasswordToken)
+router.post("/send-token",limiter,resetPasswordToken)
 router.post("/updatePassword",resetPassword)
 
 export default router;
