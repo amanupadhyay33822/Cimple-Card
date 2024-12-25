e87a2ff7354258d1f4b4d20fa1b10f7426a12219
Nitin Yadav <108917474+SoftwareDeveloperYadavJi@users.noreply.github.com>
2024-12-25 14:12:26 +0530

    Incres this limit

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,9 @@ app.use(
 	})
   );
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true , limit: "50mb" }));
+
 app.use("/api/user", UserRoutes);
 app.use("/api/card", cardRoutes);
 app.use("/api/appointment", appointmentRoutes);
